8d6ac185512d748206d244d71cfc0c7c51f1baf6
Esteban <xpitufo197@gmail.com>
2020-05-31 10:52:44 -0500

    comit preventivo

--- build/generated-sources/ap-source-output/Entidades/Movimiento_.java
+++ build/generated-sources/ap-source-output/Entidades/Movimiento_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-29T16:35:33")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-31T00:24:49")
 @StaticMetamodel(Movimiento.class)
 public class Movimiento_ { 
 
